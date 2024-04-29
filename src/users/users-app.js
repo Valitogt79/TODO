cebbe087f135472d81467b3e5c256cf118f895d3
Oswaldo Alonzo <oalonzo@gmail.com>
2024-04-29 10:41:10 -0600

    ADD: componentes y estructura de model

--- src/users/users-app.js
+++ src/users/users-app.js
@@ -2,6 +2,7 @@ import usersStore from './store/users-store';
 import { renderTable } from './presentetion/render-table/render-table';
 import { reenderButtons } from './presentetion/render-buttons/reder-buttons';
 import { renderAddButton } from './presentetion/render-add-button/render-add-button';
+import { renderModal } from './presentetion/render-modal/render-modal';
 
 /**
  * 
@@ -15,4 +16,5 @@ export const UsersApp = async (element) => {
   renderTable( element );
   reenderButtons( element );
   renderAddButton( element );
+  renderModal(element );
 };
